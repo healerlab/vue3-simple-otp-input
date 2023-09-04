9a6d436815ccb5d91e2ce7bc2693f1d63a86a9c5
HealerNguyen <nguyenductuan98tb@gmail.com>
2023-09-04 20:50:03 +0700

    deploy

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/',
+  base: '/vue3-simple-otp-input/',
   lang: "en-US",
   lastUpdated: true,
   cleanUrls: true,
