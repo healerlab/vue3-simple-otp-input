f57179d293b20dffa0bc275ae134c329056fc34c
HealerNguyen <nguyenductuan98tb@gmail.com>
2023-09-04 20:20:37 +0700

    fix: deploy

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/vue3-simple-otp-input/',
   lang: "en-US",
   lastUpdated: true,
   cleanUrls: true,
